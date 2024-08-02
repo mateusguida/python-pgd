2555be826032e8837051bbf5050670bff63734d2
mateusguida <guidamaster@gmail.com>
2024-08-02 18:40:36 -0300

    Atualizando codificação do txt

--- 16_exportanotas.py
+++ 16_exportanotas.py
@@ -20,5 +20,5 @@ lista = []
 for i in range(len(notas)):
     lista.append(f"A nota da {disciplinas[i]} foi {notas[i]}\n")
 
-with open("boletim.txt", 'a') as arq:
+with open("boletim.txt", 'a', encoding='utf-8') as arq:
     arq.writelines(lista)
