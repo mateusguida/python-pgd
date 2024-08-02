be60a4a340d56796ffb6558b3402afbc47c5c5df
mateusguida <guidamaster@gmail.com>
2024-08-02 19:52:27 -0300

    Exercício de forca atualizado e 100% funcional

--- exercicios/ex002-forca.py
+++ exercicios/ex002-forca.py
@@ -4,28 +4,40 @@ palavras = ["abacaxi", "banana", "carambola", "damasco", "embauba"]
 
 sorteada = palavras[random.randint(0,4)]
 forca = ['-'] * len(sorteada)
+letras = ['-'] * 26
 erros = 0
 acertos = 0
 
 while True:
-  chute = input("Escolha uma letra: ")
-  pos = 0
+  chute = input("Escolha uma letra: ").lower()
   flag = True #começa com verdadeiro, para checar se foi um chute errado
-  for letra in sorteada:
-    if letra == chute:
-      forca[pos] = chute
-      acertos += 1
-      flag = False #caso pelo menos um chute seja certo, a flag é modificada
-    pos += 1
+
+  #checando se a letra já foi escolhida
+  asc = ord(chute)-97
+  if(letras[asc] == '-'):
+    letras[asc] = chute #salvando a letra escolhida
+    pos = 0
+    # verificando se tem a letra na palavra
+    for letra in sorteada:
+      if letra == chute:
+        forca[pos] = chute
+        flag = False
+      pos += 1
+  else:
+    print("Você já chutou essa letra")
 
   if(flag):
     erros += 1
-  
-  if(acertos == len(sorteada)):
-    print(f"Parabéns! Você sobreviveu a forca! A palavra era {sorteada.upper()}")
-    break
-  elif(erros > 5):
+
+  print("=============")
+  print(forca)
+  print("=============")
+  print("Letras já escolhidas: ", letras)
+
+  if(erros > 5):
     print(f"Eita! Você foi enforcado! A palavra era {sorteada.upper()}")
     break
 
-  print(forca)
+  elif(sorteada == ''.join(forca)):
+    print(f"Parabéns! Você sobreviveu a forca! A palavra era {sorteada.upper()}")
+    break
