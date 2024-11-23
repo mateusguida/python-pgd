c257bcad14b36eb80e2ce2559c729534b409a1ef
mateusguida <guidamaster@gmail.com>
2024-11-22 21:20:47 -0300

    Aperfeiçoando exercício de vetores

--- exerc-nivel-2/vetor.py
+++ exerc-nivel-2/vetor.py
@@ -1,38 +1,50 @@
-def magnitude(a, b):
-  return ((a**2) + (b**2)) ** (1/2)
+# Função para calcular a magnitude de um vetor
+def magnitude(vet):
+  return ((vet(0)**2) + (vet(1)**2)) ** (1/2)
 
-def soma_vetor(a,b,c,d):
-  return a+c, b+d
+# Função para somar dois vetores
+def somar(v1,v2):
+  if len(v1) != len(v2):
+    print("Vetores incompatíveis")
+    return 0
+  res = []
+  for i in range(len(v1)):
+    res.append(v1[i] + v2[i])
+  return tuple(res)
 
-def subtr_vetor(a,b,c,d):
-  return a-c, b-d
+# Função para inverter um vetor
+def inverter(v):
+  res = []
+  for i in range(len(v)):
+    res.append(-v[i])
+  return tuple(res)
 
-def mult_escalar(a,b,k):
-  return a*k, b*k
+# Função para multiplicar dois vetores
+def subtrair(v1,v2):
+  return somar(v1, inverter(v2))
 
-def prod_escalar(a,b,c,d):
-  return a*c + b*d
+def multiplicar(vet,num):
+  res = []
+  for i in range(len(vet)):
+    res.append(vet[i] * num)
+  return tuple(res)
+
+# Função que calcula produto escalar entre dois vetores
+def prod_escalar(v1,v2):
+  if len(v1) != len(v2):
+    print("Vetores incompatíveis")
+    return None
+  soma = 0
+  for i in range(len(v1)):
+    soma += v1[i] * v2[i]
+  return soma
 
 if __name__ == '__main__':
-  # Teste Função Magnitude
-  print(magnitude(2,3)) # Resultado = 3,6055
-  print(magnitude(-1,2)) # Resultado = 2,2360
-  print(magnitude(0,-4)) # Resultado = 4,0
-  
-  # Teste Função Soma
-  x,y = soma_vetor(1,2,3,-1)
-  print(f'Vetor resultante é ({x},{y})')  # Resultado = (4,1)
-  
-  # Teste Função Subtração
-  x,y = subtr_vetor(5,4,3,1)
-  print(f'Vetor resultante é ({x},{y})')  # Resultado = (2,3)
-  
-  # Teste Função Multiplicação por Escalar
-  x,y = mult_escalar(2,3,4)
-  print(f'Vetor resultante é ({x},{y})')  # Resultado = (8,12)
-  x,y = mult_escalar(1,-2,-3)
-  print(f'Vetor resultante é ({x},{y})')  # Resultado = (-3,6)
-  
-  # Teste Função Produto Escalar
-  x = prod_escalar(2,3,4,-1)
-  print(f'O produto escalar é {x}')  # Resultado = 5
+  a = (2,5)
+  b = (1.5,7.0)
+  num = 4
+
+  print(f'Soma dos vetores {a} e {b}: ', somar(a,b))
+  print(f'Diferença dos vetores {a} e {b}: ', subtrair(a,b))
+  print(f'Multiplicação de {a} por {num}: ', multiplicar(a,num))
+  print(f'Produto escalar entre {a} e {b}: ', prod_escalar(a,b))
