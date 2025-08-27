c10ba0c6ef75bb394be72fc3c6ff5cb47f7a56f5
mateusguida <guidamaster@gmail.com>
2025-08-27 20:23:33 -0300

    Subtração, Multiplicação e Divisão de Frações

--- nivel3/fracao.py
+++ nivel3/fracao.py
@@ -5,40 +5,80 @@ import math
 
 class Fracao:
   def __init__(self, num, den):
-    self.num = num
-    self.den = den if den != 0 else 1  # evita divisão por zero
+    self.numerador = num
+    self.denominador = den if den != 0 else 1  # evita divisão por zero
 
   def __str__(self):
-    return f"{self.num}/{self.den}"
+    return f"{self.numerador}/{self.denominador}"
   
   # soma
   def __add__(self, rhs):
-    if self.den == rhs.den:
-      return Fracao(self.num + rhs.num, self.den)
+    num1, den1 = self.numerador, self.denominador
+    if isinstance(rhs, int):
+      num2, den2 = rhs, 1
     else:
-      mmc = math.lcm(self.den, rhs.den)
-      return Fracao(self.num * (mmc // self.den) + rhs.num * (mmc // rhs.den), mmc)
+      num2, den2 = rhs.numerador, rhs.denominador
+    if den1 == den2:
+      num_r = num1 + num2
+      den_r = den1
+    else:
+      den_r = den1 * den2
+      num_r = (num1 * den2) + (num2 * den1)
+    return Fracao(num_r, den_r)
   
+  # negação
+  def __neg__(self):
+    return Fracao(-self.numerador, self.denominador)
+    
+  # subtração
+  def __sub__(self, rhs):
+    return self + (-rhs)
+  
+  # multiplicação
+  def __mul__(self, rhs):
+    if isinstance(rhs, int):
+      num2, den2 = rhs, 1
+    else:
+      num2, den2 = rhs.numerador, rhs.denominador
+    num = self.numerador * num2
+    den = self.denominador * den2
+    return Fracao(num, den)
+  
+  # divisão
+  def __truediv__(self, rhs):
+    if isinstance(rhs, int):
+      num, den = 1, rhs
+    else:
+      num, den = rhs.denominador, rhs.numerador
+    return self * Fracao(num, den)
+
   # comparador ==, !=
-  def __eq__(self, rhs): # right hand side
-    if (self.den == rhs.den):
-      return self.num == rhs.num
-    else:
-      return self.num * rhs.den == rhs.num * self.den
+  def __eq__(self, rhs):
+    num1, den1 = self.numerador, self.denominador
+    num2, den2 = rhs.numerador, rhs.denominador
+    return num1 * den2 == num2 * den1
   
-  # comparador >, <
+  # comparador <, >
+  def __lt__(self, rhs):
+    num1, den1 = self.numerador, self.denominador
+    num2, den2 = rhs.numerador, rhs.denominador
+    num1_r = num1 * den2
+    num2_r = num2 * den1
+    return num1_r < num2_r
+    
   def __gt__(self, rhs):
-    if (self.den == rhs.den):
-      return self.num > rhs.num
-    else:
-      return self.num * rhs.den > rhs.num * self.den
+    num1, den1 = self.numerador, self.denominador
+    num2, den2 = rhs.numerador, rhs.denominador
+    num1_r = num1 * den2
+    num2_r = num2 * den1
+    return num1_r > num2_r
   
-  # comparador >=, <=
+  # comparador <=, >=
+  def __le__(self, rhs):
+    return self < rhs or self == rhs
+    
   def __ge__(self, rhs):
-    if (self.den == rhs.den):
-      return self.num >= rhs.num
-    else:
-      return self.num * rhs.den >= rhs.num * self.den
+    return self > rhs or self == rhs
 
 def ler_fracao():
   numerador = int(input("Digite o numerador da fração: "))
@@ -46,11 +86,18 @@ def ler_fracao():
   return Fracao(numerador, denominador)
 
 if __name__ == '__main__':
-  #fracao1 = Fracao(1, 3)
-  fracao1 = ler_fracao()
-  print(fracao1)
+  # fracao = Fracao(1, 3)
+    fracao1 = ler_fracao()
+    fracao2 = ler_fracao()
+    # print(fracao.numerador, fracao.denominador)
+    # print("A fração é:", fracao)
 
-  fracao2 = Fracao(2, 6)
-  print(fracao2)
-
-  print(fracao1 > fracao2)
+    print(f"A soma das fracoes {fracao1} e {fracao2} é", fracao1 + fracao2)
+    print(f"A diferença das fracoes {fracao1} e {fracao2} é", fracao1 - fracao2)    
+    print(f"O produto das fracoes {fracao1} e {fracao2} é", fracao1 * fracao2)
+    print(f"{fracao1} dividido por {fracao2} é", fracao1 / fracao2)
+    print(f"A fração {fracao1} é menor que {fracao2}?", fracao1 < fracao2)
+    print(f"A fração {fracao1} é maior que {fracao2}?", fracao1 > fracao2)
+    print(f"A fração {fracao1} é igual a {fracao2}?", fracao1 == fracao2)
+    print(f"A fração {fracao1} é menor ou igual a {fracao2}?", fracao1 <= fracao2)
+    print(f"A fração {fracao1} é maior ou igual a {fracao2}?", fracao1 >= fracao2)
